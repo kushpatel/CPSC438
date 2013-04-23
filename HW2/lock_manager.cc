f89706333db67ce55adb5c04576b774bf01f8d2d
kushpatel <kushpatel90@gmail.com>
2013-04-23 01:40:53 -0400

    first commit

---
+++ HW2/lock_manager.cc
@@ -0,0 +1,200 @@
+// Author: Alexander Thomson (thomson@cs.yale.edu)
+//
+// Lock manager implementing deterministic two-phase locking as described in
+// 'The Case for Determinism in Database Systems'.
+
+#include "txn/lock_manager.h"
+#include <assert.h>
+
+LockManagerA::LockManagerA(deque<Txn*>* ready_txns) {
+  ready_txns_ = ready_txns;
+}
+
+
+bool LockManagerA::WriteLock(Txn* txn, const Key& key) {
+  // Make a new LockRequest
+  LockRequest l(EXCLUSIVE, txn);
+
+  // Initialize the locktable for that key
+  if (lock_table_.count(key)) {
+    lock_table_[key]->push_back(l);
+  } else {
+    deque<LockRequest> *my_queue = new deque<LockRequest>(1, l);
+    lock_table_[key] = my_queue;
+  }
+
+  // Check if the transaction acquired the lock
+  if (lock_table_[key]->size() == 1) {
+    return true;
+  } else {
+    if (txn_waits_.count(txn)) {
+      txn_waits_[txn] = 1;
+    } else {
+     txn_waits_[txn]++;
+    }
+    return false;
+  }
+}
+
+
+bool LockManagerA::ReadLock(Txn* txn, const Key& key) {
+  // Since Part 1A implements ONLY exclusive locks, calls to ReadLock can
+  // simply use the same logic as 'WriteLock'.
+  return WriteLock(txn, key);
+}
+
+
+void LockManagerA::Release(Txn* txn, const Key& key) {
+  // Whether the removed trasaction had a lock
+  bool hadLock;
+
+  // The transaction requests for the key
+  deque<LockRequest> *requests = lock_table_[key];
+
+  // Remove the txn from requests
+  deque<LockRequest>::iterator i;
+  for (i = requests->begin(); i != requests->end(); i++) {
+    if (i->txn_ == txn) {
+      hadLock = (requests->front().txn_ == txn);
+      requests->erase(i);
+      break;
+    }
+  }
+
+  // Start the next txn if it acquired the lock
+  if (requests->size() >= 1 && hadLock) {
+    Txn *to_start = requests->front().txn_;
+    if (--txn_waits_[to_start] == 0) ready_txns_->push_back(to_start);
+  }
+}
+
+
+LockMode LockManagerA::Status(const Key& key, vector<Txn*>* owners) {
+  // reinitialize the owners vector
+  deque<LockRequest>::iterator i;
+  owners->clear();
+
+  // fill the owners vector
+  if (lock_table_[key]->size() != 0)
+    owners->push_back(lock_table_[key]->begin()->txn_);
+
+  // report status to user
+  return owners->empty() ? UNLOCKED : EXCLUSIVE;
+}
+
+
+LockManagerB::LockManagerB(deque<Txn*>* ready_txns) {
+  ready_txns_ = ready_txns;
+  assert(ready_txns_->size() == 0);
+}
+
+
+bool LockManagerB::WriteLock(Txn* txn, const Key& key) {
+  // Initialize the deque if it doesn't exist
+  LockRequest l(EXCLUSIVE, txn);
+  if (lock_table_.count(key)) {
+    lock_table_[key]->push_back(l);
+  } else {
+    deque<LockRequest> *my_queue = new deque<LockRequest>(1, l);
+    lock_table_[key] = my_queue;
+  }
+
+  if (lock_table_[key]->front().txn_ == txn) {
+    // since it's the only transaction that wants the lock, say ok to start
+    return true;
+  } else {
+    // initialize or increment the number of locks to wait for
+    if (txn_waits_.count(txn))
+      txn_waits_[txn] = 1;
+    else
+      txn_waits_[txn]++;
+    return false;
+  }
+}
+
+
+bool LockManagerB::ReadLock(Txn* txn, const Key& key) {
+  // Make a new LockRequest
+  LockRequest l(SHARED, txn);
+
+  // Initialize lock_table_
+  if (lock_table_.count(key)) {
+    lock_table_[key]->push_back(l);
+  } else {
+    deque<LockRequest> *my_queue = new deque<LockRequest>(1, l);
+    lock_table_[key] = my_queue;
+  }
+
+  // Increment position in txn_waits_ if lock is not acquired
+  deque<LockRequest> *requests = lock_table_[key];
+  deque<LockRequest>::iterator i;
+  for (i = requests->begin(); i != requests->end(); i++) {
+    if (i->mode_ == EXCLUSIVE) {
+      if (txn_waits_.count(txn))
+        txn_waits_[txn] = 1;
+      else
+        txn_waits_[txn]++;
+      return false;
+    }
+  }
+  return true;
+}
+
+
+void LockManagerB::Release(Txn* txn, const Key& key) {
+  // Lock requests for the key
+  deque<LockRequest> *requests = lock_table_[key];
+
+  // Remove the txn from the requests list
+  deque<LockRequest>::iterator i;
+  for (i = requests->begin(); i != requests->end(); i++) {
+    // Transaction was found
+    if (i->txn_ == txn) {
+      if ((i+1) != requests->end()) {
+        bool atStart = i == requests->begin();
+
+        // _SE -> start E
+        // _EE -> start E
+        if (atStart && ((i+1)->mode_ == EXCLUSIVE)) {
+          if (--txn_waits_[(i+1)->txn_] == 0)
+            ready_txns_->push_back((i+1)->txn_);
+        }
+
+        // _ES -> start longest substring of sses
+        // SES -> start longest substring of sses
+        if ((i->mode_ == EXCLUSIVE) && (
+          (atStart && (i+1)->mode_ == SHARED) ||
+          ((!atStart && (i-1)->mode_ == SHARED) && ((i+1)->mode_ == SHARED)))) {
+              deque<LockRequest>::iterator j;
+            for (j = i + 1; j != requests->end() && j->mode_ == SHARED; j++)
+              if (--txn_waits_[j->txn_] == 0) ready_txns_->push_back(j->txn_);
+        }
+      }
+      requests->erase(i);
+      break;
+    }
+  }
+}
+
+
+LockMode LockManagerB::Status(const Key& key, vector<Txn*>* owners) {
+  // Initialize owners vector
+  owners->clear();
+
+  // Nothing to do
+  if (lock_table_[key]->size() == 0) return UNLOCKED;
+
+  // Fill the vector with the transaction with an exclusive lock
+  if (lock_table_[key]->begin()->mode_ == EXCLUSIVE) {
+    owners->push_back(lock_table_[key]->begin()->txn_);
+    return EXCLUSIVE;
+  }
+
+  // Fill the vector with transactions with shared locks
+  deque<LockRequest>::iterator i;
+  deque<LockRequest> *requests = lock_table_[key];
+  for (i = requests->begin(); i != requests->end() && i->mode_ == SHARED; i++) {
+    owners->push_back(i->txn_);
+  }
+  return SHARED;
+}
---
+++ HW2/txn_processor.cc
@@ -0,0 +1,352 @@
+// Author: Alexander Thomson (thomson@cs.yale.edu)
+// Modified by: Christina Wallin (christina.wallin@yale.edu)
+
+#include "txn/txn_processor.h"
+#include <stdio.h>
+
+#include <set>
+#include <utility>
+
+#include "txn/lock_manager.h"
+
+// Thread & queue counts for StaticThreadPool initialization.
+#define THREAD_COUNT 100
+#define QUEUE_COUNT 10
+
+TxnProcessor::TxnProcessor(CCMode mode)
+    : mode_(mode), tp_(THREAD_COUNT, QUEUE_COUNT), next_unique_id_(1) {
+  if (mode_ == LOCKING_EXCLUSIVE_ONLY)
+    lm_ = new LockManagerA(&ready_txns_);
+  else if (mode_ == LOCKING)
+    lm_ = new LockManagerB(&ready_txns_);
+
+  // Start 'RunScheduler()' running as a new task in its own thread.
+  tp_.RunTask(
+        new Method<TxnProcessor, void>(this, &TxnProcessor::RunScheduler));
+}
+
+TxnProcessor::~TxnProcessor() {
+  if (mode_ == LOCKING_EXCLUSIVE_ONLY || mode_ == LOCKING)
+    delete lm_;
+}
+
+void TxnProcessor::NewTxnRequest(Txn* txn) {
+  // Atomically assign the txn a new number and add it to the incoming txn
+  // requests queue.
+  mutex_.Lock();
+  txn->unique_id_ = next_unique_id_;
+  next_unique_id_++;
+  txn_requests_.Push(txn);
+  mutex_.Unlock();
+}
+
+Txn* TxnProcessor::GetTxnResult() {
+  Txn* txn;
+  while (!txn_results_.Pop(&txn)) {
+    // No result yet. Wait a bit before trying again (to reduce contention on
+    // atomic queues).
+    sleep(0.000001);
+  }
+  return txn;
+}
+
+void TxnProcessor::RunScheduler() {
+  switch (mode_) {
+    case SERIAL:                 RunSerialScheduler();
+    case LOCKING:                RunLockingScheduler();
+    case LOCKING_EXCLUSIVE_ONLY: RunLockingScheduler();
+    case OCC:                    RunOCCScheduler();
+    case P_OCC:                  RunOCCParallelScheduler();
+  }
+}
+
+void TxnProcessor::RunSerialScheduler() {
+  Txn* txn;
+  while (tp_.Active()) {
+    // Get next txn request.
+    if (txn_requests_.Pop(&txn)) {
+      // Execute txn.
+      ExecuteTxn(txn);
+
+      // Commit/abort txn according to program logic's commit/abort decision.
+      if (txn->Status() == COMPLETED_C) {
+        ApplyWrites(txn);
+        txn->status_ = COMMITTED;
+      } else if (txn->Status() == COMPLETED_A) {
+        txn->status_ = ABORTED;
+      } else {
+        // Invalid TxnStatus!
+        DIE("Completed Txn has invalid TxnStatus: " << txn->Status());
+      }
+
+      // Return result to client.
+      txn_results_.Push(txn);
+    }
+  }
+}
+
+void TxnProcessor::RunLockingScheduler() {
+  Txn* txn;
+  while (tp_.Active()) {
+    // Start processing the next incoming transaction request.
+    if (txn_requests_.Pop(&txn)) {
+      int blocked = 0;
+      // Request read locks.
+      for (set<Key>::iterator it = txn->readset_.begin();
+           it != txn->readset_.end(); ++it) {
+        if (!lm_->ReadLock(txn, *it))
+          blocked++;
+      }
+
+      // Request write locks.
+      for (set<Key>::iterator it = txn->writeset_.begin();
+           it != txn->writeset_.end(); ++it) {
+        if (!lm_->WriteLock(txn, *it))
+          blocked++;
+      }
+
+      // If all read and write locks were immediately acquired, this txn is
+      // ready to be executed.
+      if (blocked == 0)
+        ready_txns_.push_back(txn);
+    }
+
+    // Process and commit all transactions that have finished running.
+    while (completed_txns_.Pop(&txn)) {
+      // Release read locks.
+      for (set<Key>::iterator it = txn->readset_.begin();
+           it != txn->readset_.end(); ++it) {
+        lm_->Release(txn, *it);
+      }
+      // Release write locks.
+      for (set<Key>::iterator it = txn->writeset_.begin();
+           it != txn->writeset_.end(); ++it) {
+        lm_->Release(txn, *it);
+      }
+
+      // Commit/abort txn according to program logic's commit/abort decision.
+      if (txn->Status() == COMPLETED_C) {
+        ApplyWrites(txn);
+        txn->status_ = COMMITTED;
+      } else if (txn->Status() == COMPLETED_A) {
+        txn->status_ = ABORTED;
+      } else {
+        // Invalid TxnStatus!
+        DIE("Completed Txn has invalid TxnStatus: " << txn->Status());
+      }
+
+      // Return result to client.
+      txn_results_.Push(txn);
+    }
+
+    // Start executing all transactions that have newly acquired all their
+    // locks.
+    while (ready_txns_.size()) {
+      // Get next ready txn from the queue.
+      txn = ready_txns_.front();
+      ready_txns_.pop_front();
+
+      // Start txn running in its own thread.
+      tp_.RunTask(new Method<TxnProcessor, void, Txn*>(
+            this,
+            &TxnProcessor::ExecuteTxn,
+            txn));
+    }
+  }
+}
+
+void TxnProcessor::RunOCCScheduler() {
+  Txn* txn;
+  while (tp_.Active()) {
+    // Start processing the next incoming transaction request.
+    if (txn_requests_.Pop(&txn)) {
+      txn->occ_start_time_ = GetTime();
+      tp_.RunTask(new Method<TxnProcessor, void, Txn*>(
+            this,
+            &TxnProcessor::ExecuteTxn,
+            txn));
+    }
+
+    // Verify all completed transactions
+    bool verified = true;
+    while (completed_txns_.Pop(&txn)) {
+      // check for overlap in readset
+      for (set<Key>::iterator it = txn->readset_.begin();
+           it != txn->readset_.end(); ++it) {
+        // if last modified > my start then invalid
+        if (storage_.Timestamp(*it) > txn->occ_start_time_) {
+          verified = false;
+          break;
+        }
+      }
+
+      // check for overlap in writeset
+      for (set<Key>::iterator it = txn->writeset_.begin();
+           it != txn->writeset_.end(); ++it) {
+        // if last modified > my start then invalid
+        if (storage_.Timestamp(*it) > txn->occ_start_time_) {
+          verified = false;
+          break;
+        }
+      }
+
+      // Commit/abort txn according to program logic's commit/abort decision.
+      if (txn->Status() == COMPLETED_C) {
+        if (verified) {
+          // Everything is hunky dory
+          ApplyWrites(txn);
+        } else {
+          // Try transaction again
+          NewTxnRequest(txn);
+          continue;
+        }
+      } else if (txn->Status() == COMPLETED_A) {
+        txn->status_ = ABORTED;
+      } else {
+        // Invalid TxnStatus!
+        DIE("Completed Txn has invalid TxnStatus: " << txn->Status());
+      }
+
+      // Return result to client.
+      txn_results_.Push(txn);
+    }
+  }
+}
+
+// rate at which to start threads for transaction validation
+#define N 2
+
+// rate at which to return invalid transactions to the request queue
+// and report to the user
+#define M 10
+
+void TxnProcessor::RunOCCParallelScheduler() {
+  Txn* txn;
+  while (tp_.Active()) {
+    // Start processing the next incoming transaction request.
+    if (txn_requests_.Pop(&txn)) {
+      txn->occ_start_time_ = GetTime();
+      tp_.RunTask(new Method<TxnProcessor, void, Txn*>(
+            this,
+            &TxnProcessor::ExecuteTxn,
+            txn));
+    }
+
+    // Restart or commit transactions
+    std::pair<Txn*, bool> p;
+    int j = 0;
+    while (j++ < M && validated_txns_.Pop(&p)) {
+      active_set_.erase(p.first);
+      if (!p.second) {
+        p.first->status_ = INCOMPLETE;
+        NewTxnRequest(p.first);
+        continue;
+      }
+
+      // Return result to client.
+      txn_results_.Push(p.first);
+    }
+
+    // Set the verified state of completed transactions
+    int i = 0;
+    while (i++ < N && completed_txns_.Pop(&txn)) {
+      set<Txn*> active_set_copy = set<Txn*>(active_set_);
+      active_set_.insert(txn);
+      tp_.RunTask(new Method<TxnProcessor, void, Txn*, set<Txn*>> (
+            this,
+            &TxnProcessor::ValidateTxn,
+            txn,
+            active_set_copy));
+    }
+  }
+}
+
+void TxnProcessor::ValidateTxn(Txn *txn, set<Txn*> active_set_copy) {
+  // ensure that status is COMPLETED_C
+  if (txn->Status() == COMPLETED_A) {
+    txn->status_ = ABORTED;
+    validated_txns_.Push(std::make_pair(txn, true));
+    return;
+  } else if (txn->Status() != COMPLETED_C) {
+    DIE("Completed Txn has invalid TxnStatus: " << txn->Status());
+  }
+
+  bool verified = true;
+
+  // check for overlap in readset
+  for (set<Key>::iterator it = txn->readset_.begin();
+       it != txn->readset_.end(); ++it) {
+    // if last modified > my start then invalid
+    if (storage_.Timestamp(*it) > txn->occ_start_time_) {
+      verified = false;
+      break;
+    }
+  }
+
+  // check for overlap in writeset
+  for (set<Key>::iterator it = txn->writeset_.begin();
+       it != txn->writeset_.end(); ++it) {
+    // if last modified > my start then invalid
+    if (storage_.Timestamp(*it) > txn->occ_start_time_) {
+      verified = false;
+      break;
+    }
+  }
+
+  // check if the writeset intersects with the read or write sets
+  // of any concurrently validating txns
+  for (set<Txn*>::iterator it = active_set_copy.begin();
+       it != active_set_copy.end(); ++it) {
+    for (set<Key>::iterator it2 = txn->writeset_.begin();
+        it2 != txn->writeset_.end(); ++it2) {
+      verified = verified && !(*it)->writeset_.count(*it2) &&
+                 !(*it)->readset_.count(*it2);
+      if (!verified) break;
+    }
+  }
+
+  if (verified) ApplyWrites(txn);
+  validated_txns_.Push(std::make_pair(txn, verified));
+}
+
+
+void TxnProcessor::ExecuteTxn(Txn* txn) {
+  // wipe reads_ and writes_
+  txn->reads_.clear();
+  txn->writes_.clear();
+
+  // Read everything in from readset.
+  for (set<Key>::iterator it = txn->readset_.begin();
+       it != txn->readset_.end(); ++it) {
+    // Save each read result iff record exists in storage.
+    Value result;
+    if (storage_.Read(*it, &result))
+      txn->reads_[*it] = result;
+  }
+
+  // Also read everything in from writeset.
+  for (set<Key>::iterator it = txn->writeset_.begin();
+       it != txn->writeset_.end(); ++it) {
+    // Save each read result iff record exists in storage.
+    Value result;
+    if (storage_.Read(*it, &result))
+      txn->reads_[*it] = result;
+  }
+
+  // Execute txn's program logic.
+  txn->Run();
+
+  // Hand the txn back to the RunScheduler thread.
+  completed_txns_.Push(txn);
+}
+
+void TxnProcessor::ApplyWrites(Txn* txn) {
+  // Write buffered writes out to storage.
+  for (map<Key, Value>::iterator it = txn->writes_.begin();
+       it != txn->writes_.end(); ++it) {
+    storage_.Write(it->first, it->second);
+  }
+
+  // Set status to committed.
+  txn->status_ = COMMITTED;
+}
